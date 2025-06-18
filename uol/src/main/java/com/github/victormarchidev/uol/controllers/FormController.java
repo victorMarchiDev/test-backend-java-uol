6da116d07f0f363e3c3cd2fb62755c720db9bb26
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-06-18 17:06:46 -0300

    create form page and controllers

--- uol/src/main/java/com/github/victormarchidev/uol/controllers/FormController.java
+++ uol/src/main/java/com/github/victormarchidev/uol/controllers/FormController.java
@@ -5,6 +5,8 @@ import com.github.victormarchidev.uol.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FormController {
@@ -12,6 +14,15 @@ public class FormController {
     @GetMapping("/formulario")
     public String exibirForm(Model model){
         model.addAttribute("usuario", new User());
+        return "formulario";
     }
+
+    @PostMapping("/formulario")
+    public String processarForm(@ModelAttribute User user, Model model){
+            model.addAttribute("mensagem", "Formulário recebido com sucesso");
+            return "resultado";
+    }
+
+
 
 }
