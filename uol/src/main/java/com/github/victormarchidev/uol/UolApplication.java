34cdfdbbb993957a0405216113707aa9759ae874
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-06-18 16:42:21 -0300

    building project

---
+++ uol/src/main/java/com/github/victormarchidev/uol/UolApplication.java
@@ -0,0 +1,13 @@
+package com.github.victormarchidev.uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UolApplication.class, args);
+    }
+
+}
---
+++ uol/src/test/java/com/github/victormarchidev/uol/UolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.victormarchidev.uol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
