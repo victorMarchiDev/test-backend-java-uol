b93f34ac404d11482f02910b36e4956daa949ff8
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-06-18 18:03:51 -0300

    correções

--- uol/src/main/java/com/github/victormarchidev/uol/controllers/FormController.java
+++ uol/src/main/java/com/github/victormarchidev/uol/controllers/FormController.java
@@ -3,6 +3,7 @@ package com.github.victormarchidev.uol.controllers;
 
 import com.github.victormarchidev.uol.model.User;
 import com.github.victormarchidev.uol.repository.UolRepository;
+import com.github.victormarchidev.uol.services.CodinomeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class FormController {
 
     private final UolRepository repository;
+    private final CodinomeService codinomeService;
 
-    public FormController(UolRepository repository) {
+    public FormController(UolRepository repository, CodinomeService codinomeService) {
         this.repository = repository;
+        this.codinomeService = codinomeService;
     }
 
     @GetMapping("/formulario")
@@ -26,9 +29,24 @@ public class FormController {
 
     @PostMapping("/formulario")
     public String processarForm(@ModelAttribute User user, Model model){
-            repository.save(user);
-            model.addAttribute("mensagem", "Formulário recebido com sucesso");
-            return "resultado";
+            try{
+                String codinome = codinomeService.obterCodinomeDisponivel(user.getGrupo());
+                if(codinome == null){
+                    model.addAttribute("mensagem", "Todos os codinomes deste grupo ja foram escolhidos");
+                    return "erro";
+                }
+
+                user.setCodinome(codinome);
+                repository.save(user);
+                model.addAttribute("user", user);
+                model.addAttribute("mensagem", "Usuário cadastrado com sucesso!");
+                return "resultado";
+
+            } catch (Exception e) {
+                e.printStackTrace();
+               model.addAttribute("mensagem", "Erro ao atribuir codinome ao usuario");
+               return "erro";
+            }
     }
 
 
